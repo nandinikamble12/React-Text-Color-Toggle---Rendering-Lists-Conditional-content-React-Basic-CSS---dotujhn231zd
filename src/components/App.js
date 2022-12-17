be4cbb44fbac49aa47372f225ed5a25e88e2a7d6
nandinikamble12 <117986493+nandinikamble12@users.noreply.github.com>
2022-12-17 17:43:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
 //code here 
-
-const [color, setColor] = useState("redColor");
-
-const HandleStyle = () => {
-  if(color === "redColor"){
-    setColor("blueColor");
-  }else{
-    setColor("redColor");
-
-  }
-
-}
-
-  return(
-    <>
-    <p className={color} >Newton School</p>
-    <button id='button' onClick={HandleStyle} >Change Style</button>
-  </>
+  return (
+    <div id="main">
+      <p className={} >Newton School</p>
+      <button id='button' onClick={}>Change Style</button>
+    </div>
   )
 }
 
